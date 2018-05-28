eb8b198e1841ca3e754bc36c963573fc67bd2e68
Jayon Kim <jpkim921@gmail.com>
2018-05-28 15:58:42 +0000

    automatically backed up by learn

--- db/migrate/20180528154247_create_dogs.rb
+++ db/migrate/20180528154247_create_dogs.rb
@@ -1,12 +1,19 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  #
+  # def down
+  #   drop_table :dogs
+  # end
+
+  def change
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
-  end
-
-  def down
-    drop_table :dogs
   end
 end
